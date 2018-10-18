2a280855d2b9d4518f5857f3f7ac0f306714f21d
nir94 <mateusz.s00@outlook.com>
2018-10-18 21:33:39 +0200

    Improved average temperature calculation Implemented writing temperature to a file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::fs::File;
+use std::fs::OpenOptions;
 use std::collections::LinkedList;
 use std::io::prelude::*;
 use std::{thread, time};
@@ -8,7 +9,10 @@ struct TemperatureContext {
     current: f32,
     last15sec: LinkedList<f32>,
     last60sec: LinkedList<f32>,
-    last300sec: LinkedList<f32>
+    last300sec: LinkedList<f32>,
+    last15sec_sum: f32,
+    last60sec_sum: f32,
+    last300sec_sum: f32
 }
 
 #[derive(Debug)]
@@ -22,30 +26,31 @@ struct TemperatureSnaphost {
 fn main() {
 	let interval_sec: u64 = 1;
 	let pooling_interval = time::Duration::from_millis(1000 * interval_sec);
+	let temps_file_path = String::from("/var/www/html/temps");
 
 	let mut temperature_context = TemperatureContext {
 		current: 0.0,
 		last15sec: LinkedList::new(),
 		last60sec: LinkedList::new(),
-		last300sec: LinkedList::new()
+		last300sec: LinkedList::new(),
+		last15sec_sum: 0.0,
+		last60sec_sum: 0.0,
+		last300sec_sum: 0.0
 	};
 
 	// Main loop
 	loop {
-		let temp: f32 = match read_temperature() {
-			Ok(current_temperature) => {
-				// println!("{:?}", current_temperature);
-				current_temperature
-			},
+		let current_temp: f32 = match read_temperature() {
+			Ok(t) => t,
 			Err(e) => {
 				println!("Failed to read temperature, reason: {}", e);
 				continue;
 			}
 		};
-		update_context(interval_sec, temp, &mut temperature_context);
+		update_context(interval_sec, current_temp, &mut temperature_context);
 		let snapshot: TemperatureSnaphost = get_temperature_snapshot(& temperature_context);
 		println!("{:?}", snapshot);
-
+		write_snapshot_to_file(& snapshot, & temps_file_path);
 		thread::sleep(pooling_interval);
 	}
 }
@@ -82,39 +87,46 @@ fn update_context(
 	context.last60sec.push_back(current_temperature);
 	context.last300sec.push_back(current_temperature);
 
+	context.last15sec_sum += current_temperature;
+	context.last60sec_sum += current_temperature;
+	context.last300sec_sum += current_temperature;
+
 	if context.last15sec.len() > (15 / interval_sec) as usize {
-		context.last15sec.pop_front();
+		let oldest_val = context.last15sec.pop_front().unwrap();
+		context.last15sec_sum -= oldest_val;
 	}
 
 	if context.last60sec.len() > (60 / interval_sec) as usize {
-		context.last60sec.pop_front();
+		let oldest_val = context.last60sec.pop_front().unwrap();
+		context.last60sec_sum -= oldest_val;
 	}
 
 	if context.last300sec.len() > (300 / interval_sec) as usize {
-		context.last300sec.pop_front();
+		let oldest_val = context.last300sec.pop_front().unwrap();
+		context.last300sec_sum -= oldest_val;
 	}
 }
 
-fn get_temperature_snapshot(temperature_context: & TemperatureContext) -> TemperatureSnaphost {
-	let mut sum_last15sec: f32 = 0.0;
-	for result in temperature_context.last15sec.iter() {
-		sum_last15sec += result;
-	}
-
-	let mut sum_last60sec: f32 = 0.0;
-	for result in temperature_context.last60sec.iter() {
-		sum_last60sec += result;
-	}
-
-	let mut sum_last300sec: f32 = 0.0;
-	for result in temperature_context.last300sec.iter() {
-		sum_last300sec += result;
-	}
-
+fn get_temperature_snapshot(context: & TemperatureContext) -> TemperatureSnaphost {
 	TemperatureSnaphost {
-		current: temperature_context.current,
-		last15sec_avg: sum_last15sec as f32 / temperature_context.last15sec.len() as f32,
-		last60sec_avg: sum_last60sec as f32 / temperature_context.last60sec.len() as f32,
-		last300sec_avg: sum_last300sec as f32 / temperature_context.last300sec.len() as f32
+		current: context.current,
+		last15sec_avg: context.last15sec_sum as f32 / context.last15sec.len() as f32,
+		last60sec_avg: context.last60sec_sum as f32 / context.last60sec.len() as f32,
+		last300sec_avg: context.last300sec_sum as f32 / context.last300sec.len() as f32
+	}
+}
+
+fn write_snapshot_to_file(snapshot: & TemperatureSnaphost, path_to_write: & String) {
+	let oppening_result = OpenOptions::new().write(true).truncate(true).open(path_to_write);
+	match oppening_result {
+		Ok(mut file) => {
+			write!(&mut file, "{} {} {}",
+				snapshot.last15sec_avg,
+				snapshot.last60sec_avg,
+				snapshot.last300sec_avg);
+		},
+		Err(e) => {
+			println!("Failed to write current temperature to file: {:?}", e);
+		},
 	}
 }
